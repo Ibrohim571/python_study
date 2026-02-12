0d886e44d02ee76d85a1feeed10df74630101efb
Ibrohim571 <ibrohim-01@list.ru>
2026-02-12 23:31:55 +0500

    pandas orqali txt file create qilishni o'rgandim, lekin boshlang'ich tarzda

--- data/sales_analysis/read_files.py
+++ data/sales_analysis/read_files.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import os
 
-result = pd.read_json('output/sales_data.json')
-print(result)
+# result = pd.read_json('output/sales_data.json')
+# print(result)
 
-result_excel = pd.read_excel('./output/sales_data.xlsx')
-print(result_excel)
+# result_excel = pd.read_excel('./output/sales_data.xlsx')
+# print(result_excel)
+
+os.makedirs('./output', exist_ok=True)
+
+text = pd.DataFrame(["Hello programmer"])
+
+text.to_csv("./output/example.txt", index=False)
+
+# with open('./output/sales_data.xlsx', 'r') as f:
+#     data = excel.load(f)
+#     print(data)
