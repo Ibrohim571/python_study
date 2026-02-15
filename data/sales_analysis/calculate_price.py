00974f1a94ce0b2a2ca866d594374f4e7aee8181
Ibrohim571 <ibrohim-01@list.ru>
2026-02-16 00:22:29 +0500

    Rename calculate.py file to calculate_price.py

--- data/sales_analysis/calculate_price.py
+++ data/sales_analysis/calculate_price.py
@@ -2,7 +2,7 @@ import pandas as pd
 from helper import calculate_total, format_currency
 
 df = pd.read_csv('./data/sales.csv')
-print(df)
+
 totals = []
 for index, row in df.iterrows():
     total = calculate_total(row['quantity'], row['price'])
