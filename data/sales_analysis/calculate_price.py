262a460cb671f3f85c16c8d9633207c74fe7e9f6
Ibrohim571 <ibrohim-01@list.ru>
2026-02-16 11:55:09 +0500

    know about iterrows() function and for operator, also pd.DataFrame

--- data/sales_analysis/calculate_price.py
+++ data/sales_analysis/calculate_price.py
@@ -3,6 +3,21 @@ from helper import calculate_total, format_currency
 
 df = pd.read_csv('./data/sales.csv')
 
+print(df)
+
+example = pd.DataFrame({
+    "name": ["Ali", "Vali"],
+    "age": [20, 25],
+    "status": ["student", "teacher"]
+})
+
+for index, row in example.itertuples():
+    print(row)
+
+for index, row in df.iterrows():
+    print(row)
+    print('\n')
+
 totals = []
 for index, row in df.iterrows():
     total = calculate_total(row['quantity'], row['price'])
