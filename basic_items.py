fd5ffd48c437f5f987199eed09fcce62b24e7d53
Ibrohim571 <ibrohim-01@list.ru>
2026-02-18 23:29:48 +0500

    api cheking with class

--- basic_items.py
+++ basic_items.py
@@ -12,4 +12,14 @@ class Cat:
 
 dog = Dog("Tuzik", "Bulldog")
 
-print(dog)
+print(dog.name)
+
+cat1 = Cat("name", "beshkent", "grey")
+
+print(cat1.address)
+
+class RestApi:
+    def __init__(self, api_key, model="gpt-3.5-turbo", max_tokens=100):
+        self.api_key = api_key
+        self.model = model
+        self.max_tokens = max_tokens
